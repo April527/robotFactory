8cda1f0c7af3339fb7b18b7e79db5603fddef8a7
Alejandra Correa <alecorre1@publicisgroupe.net>
2022-03-08 09:59:04 -0500

    First test shouldOrderARobot organized and running

--- src/test/java/com/N26/robotfactory/RobotfactoryApplicationTests.java
+++ src/test/java/com/N26/robotfactory/RobotfactoryApplicationTests.java
@@ -1,13 +1,15 @@
 package com.N26.robotfactory;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RobotfactoryApplicationTests {
@@ -15,28 +17,32 @@ class RobotfactoryApplicationTests {
 	@LocalServerPort
 	private int port;
 
-	private static String requestBody = "{" +
-			"  \"components\": [\"I\",\"A\",\"D\",\"F\"]" + " }";
-
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void postOrder() throws Exception {
+	public void shouldOrderARobot() throws Exception {
 
-		Response response = given()
+		postOrder("{" +
+				"  \"components\": [\"I\",\"A\",\"D\",\"F\"]" + " }")
+				.then()
+				.assertThat()
+				.statusCode(HttpStatus.OK.value())
+				.body("order_id", notNullValue())
+				.body("total", equalTo(160.11f));
+	}
+
+	public Response postOrder(String requestBody) throws Exception {
+
+		return  given()
 				.header("Content-type", "application/json")
 				.and()
 				.body(requestBody)
 				.when()
 				.port(port)
-				.post("/orders")
-				.then()
-				.extract().response();
-
-		Assertions.assertEquals(200, response.statusCode());
+				.post("/orders");
 	}
 
 }
