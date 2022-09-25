69ddd39b2920b714627cb0d0194eb791c2288ad1
AleCorrea527 <alecorre1@publicisgroupe.net>
2022-09-25 17:39:05 -0500

    Arithmetic subarray pt 1

--- src/main/java/com/N26/robotfactory/domain/usecase/AlgorithmsUseCase.java
+++ src/main/java/com/N26/robotfactory/domain/usecase/AlgorithmsUseCase.java
@@ -1,5 +1,10 @@
 package com.N26.robotfactory.domain.usecase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class AlgorithmsUseCase {
 
     public int[][] spiralMatrix3(int RowsNumber, int ColumnsNumber, int x, int y) {
@@ -7,7 +12,7 @@ public class AlgorithmsUseCase {
                 int[][] res = new int[RowsNumber * ColumnsNumber][2];
                 int moveX = 0, moveY = 1, n = 0, tmp;
 
-                for (int j = 0; j < RowsNumber * ColumnsNumber; ++n) { //n helps control the lenght of the step [1,1,2,2,3,3....]
+                for (int j = 0; j < RowsNumber * ColumnsNumber; ++n) { //n helps control the length of the step [1,1,2,2,3,3....]
                     for (int i = 0; i < (n / 2) + 1; ++i) {
 
                         if (0 <= x && x < RowsNumber && 0 <= y && y < ColumnsNumber) {
@@ -25,4 +30,25 @@ public class AlgorithmsUseCase {
                 return res;
     }
 
+    public List<Boolean> areArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+
+        for (int i = 0 ; i < l.length ; i++){
+
+            if(l[i] < nums.length && r[i] < nums.length && l[i] < r[i]) {
+
+                int[] subArrayStream = Arrays.stream(nums, l[i], r[i] + 1)
+                        .toArray();
+
+                Arrays.sort(subArrayStream);
+
+                //TODO: Go over the sorted array, see if there's an arithmetic sequence and fill the boolean list depending on the case
+                // It can be done with an Arrays.stream().sorted().map(i -> nums(i) - nums(i+1)... or find out how to do it
+
+                String ale = "";
+            }
+        }
+
+        return new ArrayList<>();
+
+    }
 }
--- src/main/java/com/N26/robotfactory/entrypoints/AlgorithmsController.java
+++ src/main/java/com/N26/robotfactory/entrypoints/AlgorithmsController.java
@@ -5,16 +5,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AlgorithmsController {
 
     @Autowired
     AlgorithmsUseCase algorithmsUseCase;
 
-    @GetMapping("/leetCodeAlgorithms")
+    @GetMapping("/spiralMatrix3")
     public int[][] leetCodeAlgorithms () {
 
        return algorithmsUseCase.spiralMatrix3(5,6,1,4);
+
+    }
+
+    @GetMapping("/arithmeticSubarrays")
+    public List<Boolean> arithmeticSubarrays () {
+
+        int[] nums =  new int[]{4,6,5,9,3,7};
+        int[] l =  new int[]{0,0,2};
+        int[] r =  new int[]{2,3,5};
+
+        return algorithmsUseCase.areArithmeticSubarrays(nums, l, r);
 
     }
 
